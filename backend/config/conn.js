695d4f38364853c4ed3123233af3318a8d942b10
EdisonPMA <portek0111@gmail.com>
2026-07-05 07:10:58 +0200

    connection modified

--- backend/config/conn.js
+++ backend/config/conn.js
@@ -7,9 +7,16 @@ function getPool() {
   if (!pool) {
     pool = mysql.createPool({
       host: process.env.DB_HOST,
+      port: process.env.DB_PORT,      // Add this
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
+
+      ssl: {
+        minVersion: "TLSv1.2",
+        rejectUnauthorized: true,
+      },
+
       waitForConnections: true,
       connectionLimit: 10,
     });
